850028b0b60b9b8367d7f284e3d47d7f55c02e04
Samraj Subramaniam <samrajs@gmail.com>
2020-08-17 22:46:27 +0200

    First commit with image building for Postgres that creates a database conference_app

---
+++ init-user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER appuser WITH PASSWORD 'App@SamGmbH1!';
+    CREATE DATABASE conference_app;
+    GRANT ALL PRIVILEGES ON DATABASE conference_app TO appuser;
+EOSQL
