44554305530c31fb3527172bbb647077808b597d
Samraj Subramaniam <samrajs@gmail.com>
2020-10-04 15:59:49 +0200

    Make it buildable from docker compose and avoid steps to create table and insert data

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM postgres:12-alpine
 COPY *.sh /docker-entrypoint-initdb.d/
+COPY create_tables.sql .
+COPY insert_data.sql .
--- init-user.sh
+++ init-user.sh
@@ -6,3 +6,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE conference_app;
     GRANT ALL PRIVILEGES ON DATABASE conference_app TO appuser;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username appuser --dbname conference_app -f /create_tables.sql
+psql -v ON_ERROR_STOP=1 --username appuser --dbname conference_app -f /insert_data.sql
